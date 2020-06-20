7c915d1deb7f80de75b35a3304665fd258fb0714
GinoQuispe21 <gino_21quispe@outlook.com>
2020-06-20 01:33:12 -0500

    Added Save Schedule Success

---
+++ src/app/save-schedule-success/save-schedule-success.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-save-schedule-succes',
+  templateUrl: './save-schedule-success.component.html',
+  styleUrls: ['./save-schedule-success.component.css']
+})
+export class SaveScheduleSuccesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
