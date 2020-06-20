2c8b10c0ffb9b934ca7abbbaeadc8196751a6a11
Grecia Guerrero <greciaguerreroa@gmail.com>
2020-06-20 03:00:18 -0500

    FirstAdvance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { InscriptionProcessComponent } from './inscription-process/inscription-process.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatisticsComponent,
+    InscriptionProcessComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [InscriptionProcessComponent]
 })
 export class AppModule { }
