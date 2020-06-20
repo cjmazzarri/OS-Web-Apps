df73b08983c87ae98a1acb08986ff5104da669ca
cjmazzarri <cjmazzarri@gmail.com>
2020-06-19 23:21:46 -0500

    Added admin-welcome-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AdminWelcomeComponent } from './admin-welcome/admin-welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminWelcomeComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AdminWelcomeComponent]
 })
 export class AppModule { }
