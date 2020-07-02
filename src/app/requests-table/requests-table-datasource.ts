f1097a162c19477585afa7eac925ffeaef635dd2
cjmazzarri <cjmazzarri@gmail.com>
2020-07-02 00:35:19 -0500

    Added requests screen

---
+++ src/app/requests-table/requests-table-datasource.ts
@@ -0,0 +1,90 @@
+import { DataSource } from '@angular/cdk/collections';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { map } from 'rxjs/operators';
+import { Observable, of as observableOf, merge } from 'rxjs';
+
+export interface RequestsTableItem {
+  code: string;
+  name: string;
+  course: string;
+}
+
+const requests: RequestsTableItem[] = [
+  { code: 'u202014045', name: 'Ror, Empe', course: 'MO05-Natación 2'},
+  { code: 'u202042069', name: 'Milos, Ricardo', course: 'MO34-Música'}
+];
+
+/**
+ * Data source for the RequestsTable view. This class should
+ * encapsulate all logic for fetching and manipulating the displayed data
+ * (including sorting, pagination, and filtering).
+ */
+export class RequestsTableDataSource extends DataSource<RequestsTableItem> {
+  data: RequestsTableItem[] = requests;
+  paginator: MatPaginator;
+  sort: MatSort;
+
+  constructor() {
+    super();
+  }
+
+  /**
+   * Connect this data source to the table. The table will only update when
+   * the returned stream emits new items.
+   * @returns A stream of the items to be rendered.
+   */
+  connect(): Observable<RequestsTableItem[]> {
+    // Combine everything that affects the rendered data into one update
+    // stream for the data-table to consume.
+    const dataMutations = [
+      observableOf(this.data),
+      this.paginator.page,
+      this.sort.sortChange
+    ];
+
+    return merge(...dataMutations).pipe(map(() => {
+      return this.getPagedData(this.getSortedData([...this.data]));
+    }));
+  }
+
+  /**
+   *  Called when the table is being destroyed. Use this function, to clean up
+   * any open connections or free any held resources that were set up during connect.
+   */
+  disconnect() {}
+
+  /**
+   * Paginate the data (client-side). If you're using server-side pagination,
+   * this would be replaced by requesting the appropriate data from the server.
+   */
+  private getPagedData(data: RequestsTableItem[]) {
+    const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
+    return data.splice(startIndex, this.paginator.pageSize);
+  }
+
+  /**
+   * Sort the data (client-side). If you're using server-side sorting,
+   * this would be replaced by requesting the appropriate data from the server.
+   */
+  private getSortedData(data: RequestsTableItem[]) {
+    if (!this.sort.active || this.sort.direction === '') {
+      return data;
+    }
+
+    return data.sort((a, b) => {
+      const isAsc = this.sort.direction === 'asc';
+      switch (this.sort.active) {
+        case 'code': return compare(+a.code, +b.code, isAsc);
+        case 'name': return compare(a.name, b.name, isAsc);
+        case 'course': return compare(+a.course, +b.course, isAsc);
+        default: return 0;
+      }
+    });
+  }
+}
+
+/** Simple sort comparator for example ID/Name columns (for client-side sorting). */
+function compare(a: string | number, b: string | number, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+}
---
+++ src/app/requests-table/requests-table.component.ts
@@ -0,0 +1,30 @@
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import {MatTable, MatTableDataSource} from '@angular/material/table';
+import { RequestsTableDataSource, RequestsTableItem } from './requests-table-datasource';
+
+@Component({
+  selector: 'app-requests-table',
+  templateUrl: './requests-table.component.html',
+  styleUrls: ['./requests-table.component.css']
+})
+export class RequestsTableComponent implements AfterViewInit, OnInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatTable) table: MatTable<RequestsTableItem>;
+  dataSource: RequestsTableDataSource;
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['code', 'name', 'course'];
+
+  ngOnInit() {
+    this.dataSource = new RequestsTableDataSource();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+  }
+}
